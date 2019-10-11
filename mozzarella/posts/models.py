82ef2fef0f01a5911aa5f26800d1d0cfa79711f8
dhesusan <dhesusan4649@gmail.com>
2019-10-11 22:43:25 +0900

    add model

--- mozzarella/posts/models.py
+++ mozzarella/posts/models.py
@@ -5,5 +5,6 @@ from mozzarella.users.models import User
 class Post(models.Model):
     class Meta:
         db_table = 'post'
+
     user_id = models.ForeignKey(User, verbose_name="投稿者", on_delete=models.SET_NULL)
     post_text = models.CharField(max_length=1000)
