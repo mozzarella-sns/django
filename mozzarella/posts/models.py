58401830058b5a4fee2e049201ef8dd28cd731b5
dhesusan <dhesusan4649@gmail.com>
2019-10-16 22:44:56 +0900

    make template

--- mozzarella/posts/models.py
+++ mozzarella/posts/models.py
@@ -7,5 +7,6 @@ class Post(models.Model):
         db_table = 'post'
 
     user_id = models.ForeignKey(User, verbose_name="投稿者ID", on_delete=models.SET_NULL)
-    post_text = models.CharField(max_length=1000)
+    post_name = models.CharField(verbose_name='投稿名', max_length=100)
+    post_text = models.TextField(max_length=1000)
     created_date = models.DateTimeField()
--- mozzarella/users/models.py
+++ mozzarella/users/models.py
@@ -1,14 +1,12 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ImageField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    thumbnail_img = ImageField(verbose_name='サムネイル画像')
+    
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
