43019159ad9b9a2c3020d71471a633b18509c264
dhesusan <dhesusan4649@gmail.com>
2019-11-20 23:54:34 +0900

    add db_arch

--- mozzarella/users/models.py
+++ mozzarella/users/models.py
@@ -5,8 +5,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-    thumbnail_img = ImageField(verbose_name='サムネイル画像')
-    
+    thumbnail_img = ImageField(verbose_name='サムネイル画像', upload_to="images/")
+    name = CharField(_("Name of User"), blank=True, max_length=255)
+
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
